a2b9831371b0a29179e6a4265ea65555c2e1d75b
AlvinChiu <alvinhsian@gmail.com>
2016-01-10 01:22:01 +0800

    將按鈕狀態判斷獨立為helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,17 @@
 module ProductsHelper
+  def button_status_by_product(product)
+    if product.quantity > 0
+      if request.path == "/products"
+        link_to("加入購物車", add_to_cart_product_path(product), method: :post, :class => "glyphicon glyphicon-plus btn btn-danger")
+      else
+        link_to("加入購物車", add_to_cart_product_path(@product), method: :post, :class => "glyphicon glyphicon-plus btn btn-primary btn-lg btn-danger")
+      end
+    else
+      if request.path == "/products"
+        link_to("上架通知我", "#", :class => "glyphicon glyphicon-info-sign btn btn-warning")
+      else
+        link_to("上架通知我", "#", :class => "glyphicon glyphicon-info-sign btn btn-primary btn-lg btn-warning")
+      end
+    end
+  end
 end
