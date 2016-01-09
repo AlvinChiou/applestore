a593adc0536e2f75e5604964c730046243b85230
AlvinChiu <alvinhsian@gmail.com>
2016-01-09 13:12:11 +0800

    [未完成] 已加入購物車的商品不能再被加進去

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,20 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :add_to_cart]
+
   def index
     @products = Product.all
   end
 
   def show
+  end
+
+  def add_to_cart
+    current_cart.add_product_to_cart(@product)
+    redirect_to :back
+  end
+
+
+  def find_product
     @product = Product.find(params[:id])
   end
 end
