d1c947a09dda639f4169780909afc09e9b5e0770
AlvinChiu <alvinhsian@gmail.com>
2016-01-27 21:43:14 +0800

    cart_item 新增 quantity 欄位

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,8 @@ Rails.application.routes.draw do
   end
   root "products#index"
 
+  resources :items, controller: "cart_items"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
