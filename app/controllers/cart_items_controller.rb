993fe34362baab908f5f3067f0d76063ac15a814
AlvinChiu <alvinhsian@gmail.com>
2016-01-28 23:42:44 +0800

    購物車加入「數量」設計 ( 購物車不能手動更新超過庫存的數量 )

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,32 @@
+class CartItemsController < ApplicationController
+  before_action :authenticate_user!
+
+  def destroy
+    @cart = current_cart
+    @item = @cart.cart_items.find_by(product_id: params[:id])
+    @product = @item.product
+    @item.destroy
+
+    flash[:notice] = "已將 #{@product.title} 重購物車移除！"
+    redirect_to :back
+  end
+
+  def update
+    @cart = current_cart
+    @item = @cart.cart_items.find_by(product_id: params[:id])
+    @product = @item.product
+
+    if @item.product.quantity >= item_params[:quantity].to_i
+      @item.update(item_params)
+      flash[:notice] = "已更新 #{@product.title} 數量！"
+    else
+      flash[:warning] = " #{@product.title} 數量不足以加入購物車"
+    end
+    redirect_to carts_path
+  end
+
+  private
+  def item_params
+    params.require(:cart_item).permit(:quantity)
+  end
+end
