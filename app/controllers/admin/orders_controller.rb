2591564d7af094550df04569a07f24ede868482b
AlvinChiu <alvinhsian@gmail.com>
2016-02-03 01:17:38 +0800

    admin/orders/id 顯示不正常

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,8 +3,38 @@ class Admin::OrdersController < ApplicationController
 
   before_action :authenticate_user!
   before_action :admin_required
+  before_action :find_order_by_token, only: [:show, :ship, :shipped, :cancel, :return]
 
   def index
     @orders = Order.order("id DESC")
+  end
+
+  def show
+    @order_info = @order.info
+    @order_items = @order.items
+  end
+
+  def cancel
+    
+  end
+
+  def ship
+    @order.ship!
+    redirect_to :back
+  end
+
+  def shipped
+    @order.deliver!
+    redirect_to :back
+  end
+
+  def return
+    @order.return_good!
+    redirect_to :back
+  end
+  
+  private
+  def find_order_by_token
+    @order = Order.find_by_token(params[:id])
   end
 end
