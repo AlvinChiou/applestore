cef9fccca76f9197a7486aa6b264849d0c170d3f
AlvinChiu <alvinhsian@gmail.com>
2016-02-08 00:12:58 +0800

    修復購物車無法取消單筆商品問題

--- config/routes.rb
+++ config/routes.rb
@@ -42,9 +42,9 @@ Rails.application.routes.draw do
     resources :orders
   end
 
-  root "products#index"
-
   resources :items, controller: "cart_items"
+
+  root "products#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
