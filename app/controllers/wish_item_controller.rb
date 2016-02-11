e184656e0148eaa2cb8cd62608fd6db32499b605
AlvinChiu <alvinhsian@gmail.com>
2016-02-12 02:40:07 +0800

    修復願望清單無法取消商品項目問題

--- app/controllers/wish_item_controller.rb
+++ app/controllers/wish_item_controller.rb
@@ -2,7 +2,13 @@ class WishItemController < ApplicationController
   before_action :authenticate_user!
 
   def destroy
+    @wish_list = current_wish_list
+    @wish_list_item = @wish_list.find_wish_list_item(params[:id])
+    @product = @wish_list_item.product
+    @wish_list_item.destroy
 
+    flash[:warning] = "已將 #{@product.title} 從願望清單移除！"
+    redirect_to :back
   end
 
   def update
