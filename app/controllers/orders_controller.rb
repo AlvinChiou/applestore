d0a10b2b96104d6f81cbf4f46718dad3e7e535db
AlvinChiu <alvinhsian@gmail.com>
2016-02-24 00:31:52 +0800

    新增信用卡交易

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,9 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :pay2go_cc_notify
+  before_action :find_order_by_token, only: [:show, :pay_with_credit_card, :pay2go_cc_notify]
+  protect_from_forgery except: :pay2go_cc_notify
 
   def show
-    @order = Order.find_by_token(params[:id])
     @order_info = @order.info
     @order_items = @order.items
   end
@@ -26,7 +27,6 @@ class OrdersController < ApplicationController
   end
 
   def pay_with_credit_card
-    @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("credit_card")
 
     # 結帳
@@ -42,11 +42,32 @@ class OrdersController < ApplicationController
     redirect_to account_orders_path, notice: "成功完成付款"
   end
 
+  def pay2go_cc_notify
+    if params["Status"] == "SUCCESS"
+
+      @order.make_payment!
+
+      if @order.is_paid?
+        flash[:notice] = "信用卡付款成功"
+        redirect_to account_orders_path
+      else
+        render text: "信用卡失敗"
+      end
+    else
+      render text: "交易失敗"
+    end
+  end
+
   private
   def order_params
     params.require(:order).permit(info_attributes: [:billing_name,
                                                     :billing_address,
                                                     :shipping_name,
                                                     :shipping_address])
+  end
+  
+  private
+  def find_order_by_token
+    @order = Order.find_by_token(params[:id])
   end
 end
